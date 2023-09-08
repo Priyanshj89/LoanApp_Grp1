2cc143604f9756e02586134889d822afb9790ddb
unknown <vedant.barsagade@gmail.com>
2023-09-08 10:25:27 +0530

    added admin controller

---
+++ backend/src/main/java/com/example/demo/entities/Admin.java
@@ -0,0 +1,51 @@
+package com.example.demo.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="admin")
+public class Admin {
+	@Id
+	@Column(name="admin_id")
+	private String admin_id;
+	
+	@Column(name="name")
+	private String name;
+	
+	@Column(name="password")
+	private String password;
+	
+	public Admin() {
+		super();
+	}
+	
+	public Admin(String admin_id, String name, String password) {
+		super();
+		this.admin_id = admin_id;
+		this.name = name;
+		this.password = password;
+	}
+	public String getAdmin_id() {
+		return admin_id;
+	}
+	public void setAdmin_id(String admin_id) {
+		this.admin_id = admin_id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+
+}
