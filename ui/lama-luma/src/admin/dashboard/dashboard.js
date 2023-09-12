1d4be13bef4c265b7c4ac0574ea2cfae8b36b18d
unknown <vedant.barsagade@gmail.com>
2023-09-12 10:32:14 +0530

    trial2

--- ui/lama-luma/src/admin/dashboard/dashboard.js
+++ ui/lama-luma/src/admin/dashboard/dashboard.js
@@ -1,4 +1,15 @@
+import { Button } from "react-bootstrap";
+
 const Dashboard = () => {
-    return <div><h1>Admin Dashboard</h1></div>
+    return <div className="adminDashboard">
+        <h1>Admin Dashboard</h1>
+        <Button style={{marginLeft:'150px',marginTop:'200px'}}>Customer data Management</Button>
+
+        <Button style={{marginLeft:'150px',marginTop:'200px'}}>Loan Card Management</Button>
+        <Button style={{marginLeft:'150px',marginTop:'200px'}}>Item Master Data</Button>
+
+
+
+    </div>
 }
 export {Dashboard};
