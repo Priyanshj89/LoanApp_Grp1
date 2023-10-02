e23ec0fa4a693f3720f6b7ade2e6ff923067dbc5
priyanshj89 <j.priyansh89@gmail.com>
2023-10-02 13:19:17 +0530

    UI Changes

--- ui/lama-luma/src/user/dashboard/dashboard.js
+++ ui/lama-luma/src/user/dashboard/dashboard.js
@@ -1,6 +1,7 @@
 import Navbar from "../../components/Navbar";
 import "../../styles/UserDashboard.css"
 import "../../styles/EmployeeLogin.css"
+import "../../styles/AdminDashboard.css"
 import { Link } from "react-router-dom";
 
 const Dashboard = () => {
@@ -9,10 +10,10 @@ const Dashboard = () => {
         <Navbar/>
         <div className="user-dashboard">
         <h1>User Dashboard</h1>
-        <div style={{marginTop:'150px'}}>
-            <Link to ='/user/dashboard/viewLoan'><button className="prim-btn" style={{width:'30%'}}>View Your Loans</button></Link>
-            <Link to ='/user/dashboard/applyLoan'><button className="prim-btn" style={{width:'25%'}}>Apply for a Loan</button></Link>
-            <Link to ='/user/dashboard/viewItemsPurchased'><button className="prim-btn" style={{width:'25%'}}>View Items Purchased</button></Link>
+        <div className="dashboard-card-collection">
+        <div className="dashboard-card"><div className="dasboad-card-text">View your loans here and their respective status.</div><Link to ='/user/dashboard/viewLoan'><button className="dashboard-card-btn">View Your Loans</button></Link></div>
+        <div className="dashboard-card"><div className="dasboad-card-text">Apply loan for particular items existing in system.</div><Link to ='/user/dashboard/applyLoan'><button className="dashboard-card-btn">Apply for a Loan</button></Link></div>
+        <div className="dashboard-card"><div className="dasboad-card-text">View all the items Purchased by you </div> <Link to ='/user/dashboard/viewItemsPurchased'><button className="dashboard-card-btn">View Items Purchased</button></Link></div>
         </div>
         </div>
     </div>);
